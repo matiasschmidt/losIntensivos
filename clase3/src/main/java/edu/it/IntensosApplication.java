7e80714973f326b835e8df69c2f90974ad03b628
andy <arteysoft@gmail.com>
2021-05-11 20:07:17 -0300

    prueba maven

---
+++ clase3/src/main/java/edu/it/IntensosApplication.java
@@ -0,0 +1,11 @@
+package edu.it;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class IntensosApplication {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a maven");
+    }
+}
